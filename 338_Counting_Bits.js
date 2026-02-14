5f5a73c8db8926d21a8385a3a6d58a7fdc0e7def
Rahul <rahulkum94l94@gmail.com>
2026-02-14 11:25:40 +0530

    111

---
+++ 338_Counting_Bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+function countBits(n) {
+    let dp = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+        dp[i] = dp[i >> 1] + (i & 1);
+    }
+    return dp;
+}
