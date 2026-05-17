f0f2f3eaf99ec8976b1c02caeabb743fd646ac24
rahul personal <rahulkum94l94@gmail.com>
2026-05-17 12:04:59 +0530

    193

---
+++ 1978_Employees_Whose_Manager_Left_the_Company.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select employee_id 
+from Employees
+where 
+manager_id is not null and
+salary < 30000 and 
+manager_id not in (select employee_id from Employees)
